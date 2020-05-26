756e20915eded8780389e6d0da7bb61aec3e3340
Augusto Castilho <castilho@ice.ufjf.br>
2020-05-26 09:49:44 -0300

    Implementacao da funcionalidade de exclusao do produto

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -35,7 +35,26 @@ class ProductItem extends StatelessWidget {
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
               onPressed: () {
-                Provider.of<Products>(context, listen: false).deleteProduct(product.id);
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text("Confirmar exclusão"),
+                    content: Text("Quer mesmo excluir este produto?"),
+                    actions: <Widget>[
+                      FlatButton(
+                        onPressed: () => Navigator.of(context).pop(false),
+                        child: Text("Não"),
+                      ),
+                      FlatButton(
+                        onPressed: () => Navigator.of(context).pop(true),
+                        child: Text("Sim"),
+                      ),
+                    ],
+                  ),
+                ).then((value) {
+                  Provider.of<Products>(context, listen: false)
+                      .deleteProduct(product.id);
+                });
               },
             ),
           ],
